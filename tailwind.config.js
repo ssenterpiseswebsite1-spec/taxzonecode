bccc2eb9b8183cab700a8abd4805510aa63178c6
ssenterpiseswebsite1-spec <ssenterpiseswebsite1@gmail.com>
2026-02-08 23:24:20 +0530

    Fix Next.js and React versions for Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
